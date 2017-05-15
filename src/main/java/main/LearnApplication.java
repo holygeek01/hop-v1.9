6a994f573496876f318ebf03c2b302a378e3ba23
Rejin <holygeek1@gmail.com>
2017-05-16 01:16:17 +0530

    my initial commit

---
+++ src/main/java/main/LearnApplication.java
@@ -0,0 +1,25 @@
+package main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+
+@SpringBootApplication
+@Controller
+@EnableAutoConfiguration
+public class LearnApplication {
+	@RequestMapping(value={"/", "/login"}, method = RequestMethod.GET)
+	public ModelAndView login(){
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("index");
+		return modelAndView;
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(LearnApplication.class, args);
+	}
+}
